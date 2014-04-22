7deb5f024ad9e6d8b40b641a251784f7cbf81596
s9908 <s9908@pjwstk.edu.pl>
2014-04-22 20:23:05 +0200

    test

---
+++ telefonyjsp/src/org/piotrszura/telefonyjsp/Producent.java
@@ -0,0 +1,23 @@
+package org.piotrszura.telefonyjsp;
+
+public class Producent {
+	private int id;
+	private String nazwa;
+	
+	public int getId() 
+	{
+		return id;
+	}
+	public void setId(int id) 
+	{
+		this.id = id;
+	}
+	public String getNazwa() 
+	{
+		return nazwa;
+	}
+	public void setNazwa(String nazwa) 
+	{
+		this.nazwa = nazwa;
+	}	
+}
